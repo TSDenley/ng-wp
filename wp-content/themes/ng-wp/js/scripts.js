58ae1c059b846429169f5bf1f476ae4995818de4
Tom Denley <tsdenley@gmail.com>
2015-05-27 11:58:00 +0100

    Search form direcitve (not filter posts display)

--- wp-content/themes/ng-wp/js/scripts.js
+++ wp-content/themes/ng-wp/js/scripts.js
@@ -23,6 +23,33 @@
 				console.log(res);
 				$scope.posts = res;
 			});
+		})
+
+		/*
+		* Search form directive
+		*/
+		.directive('searchForm', function () {
+			return {
+				restrict: 'EA',
+				template: '<input type="search" name="s" placeholder="Search" ng-model="filter.s" ng-change="search()">',
+				controller: function ( $scope, $http ) {
+					$scope.filter = {
+						s: ''
+					};
+
+					$scope.search = function () {
+						console.log($scope.filter.s);
+
+						if ( $scope.filter.s.length > 2 ) {
+							$http
+								.get(appVars.siteURL + 'wp-json/wp/v2/posts?filter[s]=' + $scope.filter.s)
+								.success(function (res) {
+									$scope.posts = res;
+								});
+						};
+					};
+				}
+			};
 		});
 
 }());

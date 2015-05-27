81a381b9b9689704ff88a39e806f1ba4e6156eb4
Tom Denley <tsdenley@gmail.com>
2015-05-27 21:17:30 +0100

    Moved ng-app directive to fix WP admin bar

--- wp-content/themes/ng-wp/header.php
+++ wp-content/themes/ng-wp/header.php
@@ -7,7 +7,7 @@
  * @package ng-wp
  */
 ?><!DOCTYPE html>
-<html <?php language_attributes(); ?> ng-app="ng-wp">
+<html <?php language_attributes(); ?>>
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -19,7 +19,7 @@
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="hfeed site">
+<div id="page" class="hfeed site" ng-app="ng-wp">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'ng-wp' ); ?></a>
 
 	<header id="masthead" class="site-header" role="banner">
